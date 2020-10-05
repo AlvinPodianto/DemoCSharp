acfc9176a77edcc589c08e11373d2308ed524799
Alvin Podianto <alvin@farmaku.com>
2020-10-05 17:53:11 +0700

    Add project files.

---
+++ DemoAPI/Contracts/IRepo.cs
@@ -0,0 +1,12 @@
+﻿using DemoAPI.Models;
+using System.Collections.Generic;
+
+namespace DemoAPI.Contracts
+{
+    public interface IPersonRepo
+    {
+        List<Person> GetListPerson(int page, int itemsPerPage);
+        Person GetPersonById(long id);
+        SuccessResponse AddPerson(Person person);
+    }
+}
---
+++ DemoAPI/Models/Person.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace DemoAPI.Models
+{
+    public class Person
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Occupation { get; set; }
+
+        public List<string> Emails { get; set; }
+
+        public Person()
+        {
+            Emails = new List<string>();
+        }
+    }
+
+    public class SuccessResponse
+    {
+        public bool Success { get; set; }
+        public string Reason { get; set; }
+    }
+}
